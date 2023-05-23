805fc2bcf03ef785ae928e574093849efb6cd480
RebeccaVJ <rebeccajorge@email.com>
2023-05-23 15:21:45 -0300

    construção js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,47 @@
-import cnpjConfirmado from "./confere-cnpj.js";
-const camposFormulario = document.querySelectorAll('[required]')
+// const formularioCampos = document.querySelectorAll("[required]");
 
-// formulario.addEventListener("submit", (e) => {
-//     e.preventDefault();
+// const tiposDeErros = [
+//     "valueMissing",
+//     "typeMismatch",
+//     "tooShort",
+//     "patternMismatch",
+//     "customError",
+// ];
 
-//     const tabelaRespostas = {
-//         "cnpj": e.target.elements["cnpj"].value,
-//         "razao social": e.target.elements["razao social"].value,
-//         "cpf representante legal": e.target.elements["cpf representante legal"].value,
-//         "nome do representante legal": e.target.elements["nome do representante legal?"].value,
-//         "como deseja ser chamado?": e.target.elements["como deseja ser chamado?"].value,
-//         "quantidade de funcionarios": e.target.elements["quantidade de funcionarios"].value,
-//         "faturamento anual": e.target.elements["faturamento anual"].value,
-//     }
-
-//     localStorage.setItem("cadastro", JSON.stringify(tabelaRespostas));
-
-//     window.location.href = "./confere.html";
-// })
-
-camposFormulario.forEach((campo) => {
-    campo.addEventListener("blur", () => verificandoCampos(campo));
-})
-
-function verificandoCampos(campo) {
-    if (campo.name == "cnpj" && campo.value.length >= 14 ) {
-        cnpjConfirmado(campo);
-    }
-}
+// const mensagensDeErro = {
+//     cpnj: {
+//         valueMissing: "cnpj inválido",
+//         tooShort: "cnpj inválido",
+//         patternMismatch: "cnpj inválido",
+//     },
+//     razaoSocial: {
+//         valueMissing: "tamanho máximo de 30 caracteres",
+//         tooShort: "tamanho máximo de 30 caracteres",
+//         patternMismatch: "tamanho máximo de 30 caracteres",
+//     },
+//     cpf: {
+//         valueMissing: "cpf inválido",
+//         tooShort: "cpf inválido",
+//         patternMismatch: "cpf inválido",
+//     },
+//     nomeRepresentante: {
+//         valueMissing: "tamanho máximo de 20 caracteres",
+//         tooShort: "tamanho máximo de 20 caracteres",
+//         patternMismatch: "tamanho máximo de 20 caracteres",
+//     },
+//     comoDesejaSerChamado: {
+//         valueMissing: "tamanho máximo de 10 caracteres",
+//         tooShort: "tamanho máximo de 10 caracteres",
+//         patternMismatch: "tamanho máximo de 10 caracteres",
+//     },
+//     quantidadeFuncionarios: {
+//         valueMissing: "campo obrigatório",
+//         tooShort: "campo obrigatório",
+//         patternMismatch: "campo obrigatório",
+//     },
+//     faturamentoAnual: {
+//         valueMissing: "campo obrigatório",
+//         tooShort: "campo obrigatório",
+//         patternMismatch: "campo obrigatório",
+//     },
+// };
